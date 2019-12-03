4402bc03dedd3e5e99a5f3076fa7260042b8f101
Pratik Shende <pratikshende99@gmail.com>
2019-12-03 10:56:43 +0530

    adding a changes from newUpdate branch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import reactDOM from "react-dom";
 import App from "./App";
-
+import Appp from "./Appp";
 reactDOM.render(<App />, document.querySelector("#root"));
+console.log("newUpdateBranch");
